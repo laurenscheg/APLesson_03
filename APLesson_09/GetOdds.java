21f4d0627d8a05500e7127deb48a78157aa55ac5
laurenscheg <laurenscheg22@gmail.com>
2016-11-29 09:18:42 -0800

    .

---
+++ APLesson_09/GetOdds.java
@@ -0,0 +1,17 @@
+public class GetOdds
+{
+	static int[] num;
+	public static void main(String[]args)
+	{
+		num = new int[10];
+		fillArray();
+	
+
+	}
+	
+	public static int fillArray(int[] num);
+	{
+		int number = 1 + (int)((Math.random()*100));
+		
+	}
+}	
