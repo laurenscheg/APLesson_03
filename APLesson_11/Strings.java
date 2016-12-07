da14557c4579f46accd8b12fd11bf6737022b07f
laurenscheg <laurenscheg22@gmail.com>
2016-12-07 08:47:57 -0800

    .

---
+++ APLesson_11/Strings.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Strings
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		
+		String[][]words = new String[4][4];
+		
+		for(int i = 0; i < words.length; i++)
+		{
+			for(int j = 0; j < words[i].length; j++)
+			{
+				System.out.println("Enter a word: ");
+				String word = kb.next();
+				words[i][j] = word;
+			}
+		}
+		
+		for(int i = 0; i < words.length; i++)
+		{
+			for(int j = 0; j < words[i].length; j++)
+			{
+				System.out.print(words[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		
+	}
+}
