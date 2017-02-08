3f02b54210ea9ba9fd28fa65863379eeaead08d6
laurenscheg <laurenscheg22@gmail.com>
2017-02-08 10:25:07 -0800

    .

---
+++ APLesson_13/ToyRunner.java
@@ -0,0 +1,11 @@
+public class ToyRunner
+{
+	public static void main(String[]args)
+	{
+		Car toy1 = new Car("Lightning McQueen");
+		AFigure toy2 = new AFigure("Mater");
+		
+		System.out.println(toy1.toString());
+		System.out.println(toy2.toString());
+	}
+}
